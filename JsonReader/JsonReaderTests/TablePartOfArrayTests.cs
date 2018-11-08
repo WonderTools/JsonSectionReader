1080d4f5ecc507f17d2a8a52964dc90fde7006d8
Nachiappan <nachiapan@gmail.com>
2018-11-08 16:01:31 +0530

    Added test case for accessing from the array

---
+++ JsonReader/JsonReaderTests/TablePartOfArrayTests.cs
@@ -0,0 +1,19 @@
+using FluentAssertions;
+using JsonReader;
+using NUnit.Framework;
+
+namespace JsonReaderTests
+{
+    [TestFixture]
+    public class TablePartOfArrayTests
+    {
+        [Test]
+        public void Tests()
+        {
+            var reader = new WtJsonReader();
+            var actual = reader.Read("TablePartOfArrayTests.json", "node1", 1).GetTable(typeof(int), typeof(int));
+            var expected = reader.Read("TablePartOfArrayTests.json", "node2").GetTable(typeof(int), typeof(int));
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
