a5b2a1af1097e232384843f866f9a4870aa31029
Nachiappan <nachiapan@gmail.com>
2018-11-26 05:39:13 +0100

    Basic Data Tests

---
+++ JsonReader/JsonReaderTests/DataTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+using WonderTools.JsonReader;
+
+namespace WonderTools.JsonReaderTests
+{
+    public class DataTests
+    {
+        public static IEnumerable<TestCaseData> TestCases()
+        {
+            yield return new TestCaseData(".DataTests.json", "number", 34850924).SetName("Reading a number as int");
+        }
+
+        [TestCaseSource(nameof(TestCases))]
+        public void Test(string fileName, string sectionName, object expected)
+        {
+            var section = new JsonSectionReader().Read(fileName, Encoding.UTF8).GetSection(sectionName);
+            var result = section.GetObject<int>();
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
