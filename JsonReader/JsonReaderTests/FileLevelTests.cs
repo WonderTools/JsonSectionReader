b0a15166f5b27f7769ceda182bfcb4d15094db6d
Nachiappan <nachiapan@gmail.com>
2018-11-27 07:54:29 +0100

    Fixed a test case that was broken during refactoring

--- JsonReader/JsonReaderTests/FileLevelTests.cs
+++ JsonReader/JsonReaderTests/FileLevelTests.cs
@@ -13,7 +13,7 @@ namespace WonderTools.JsonSectionReaderTests
         [TestCase("InvalidFile.json", typeof(EmbeddedResourceNotFoundException),
             "No embedded resource with name ending with InvalidFile.json was found")]
         [TestCase("Test.json", typeof(MultipleResourceFoundException),
-            "Multiple file with name Test.json found : WonderTools.JsonReaderTests.Folder.Test.json,WonderTools.JsonReaderTests.Test.json")]
+            "Multiple file with name Test.json found : WonderTools.JsonSectionReaderTests.Folder.Test.json,WonderTools.JsonSectionReaderTests.Test.json")]
         public void InvalidFileTests(string fileName, Type exceptionType, string errorMessage)
         {
             var exception = Test.GetExceptionAttemptingToGetJsonSegment(fileName);
